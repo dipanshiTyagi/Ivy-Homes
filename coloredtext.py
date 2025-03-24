60e601b48e0a8240a4dba222ef9093e3ef241a50
dipanshiTyagi <143926420+dipanshiTyagi@users.noreply.github.com>
2025-03-24 12:43:47 +0530

    Delete file

--- coloredtext.py
+++
@@ -1,7 +0,0 @@
-import colorama
-from colorama import Fore, Back, Style
-
-colorama.init(autoreset=True)
-
-print(Fore.BLUE + Back.YELLOW + "Hi my name is Dipanshi")
-print(Back.CYAN + "Hello there")
