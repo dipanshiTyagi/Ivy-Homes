e618d9e99711b217a740e080b17a3ea56b7989b3
dipanshiTyagi <143926420+dipanshiTyagi@users.noreply.github.com>
2025-03-24 12:44:38 +0530

    Delete file

--- password_generator.py
+++
@@ -1,5 +0,0 @@
-import random
-passlen = int(input("Enter the length of password: "))
-s = "abchjfeioeklkahuffjdcvuj1w283749836281828@#^$*^%$&*AEHRFJDIEODENGVYIGHRBFYIFJMSJ"
-p = "".join(random.sample(s, passlen))
-print(p)
