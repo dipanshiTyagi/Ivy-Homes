8185948e5dab5bffeac98821a5ed4421b7c180fa
dipanshi <dipanshityagi18@gmail.com>
2025-03-24 11:43:04 +0530

    Resolved merge conflicts

--- venv/api/additional_testing.py/additional_testing.py
+++ venv/api/additional_testing.py/additional_testing.py
@@ -1,4 +1,4 @@
-import requests # type: ignore
+import requests 
 import time
 
 # Base URL of the API
--- venv/api/extracting_names.py/extractNames.py
+++ venv/api/extracting_names.py/extractNames.py
@@ -1,4 +1,4 @@
-import requests # type: ignore
+import requests 
 import time
 from collections import deque
 
